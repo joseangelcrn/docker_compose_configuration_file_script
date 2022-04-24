bf708bee9b6d20c1ed328170ecf13ca39d8dd1f3
joseangelcrn <47973568+joseangelcrn@users.noreply.github.com>
2022-04-24 19:38:36 +0200

    Update custom_docker_file_compose.sh

--- custom_docker_file_compose.sh
+++ custom_docker_file_compose.sh
@@ -2,16 +2,17 @@
 
 #Change to your correct file path..
 #--- docker-compose.yml location
-path_docker_compose_yml="/home/$USER/Documentos/docker_test/my_app"
+#path_docker_compose_yml="/home/$USER/Documentos/docker_test/my_app"
+path_docker_compose_yml="."
 #--- docker-compose-custom.yml save location
 output_path="$path_docker_compose_yml/docker-compose-custom.yml"
 
-#DEFAULT BITNAMI MARIADB VALUES
+#DEFAULT BITNAMI MARIADB VALUES (not change)
 
 default_mariadb_user="bn_myapp"
 default_mariadb_database="bitnami_myapp"
 
-#DEFAULT BITNAMI LARAVEL VALUES
+#DEFAULT BITNAMI LARAVEL VALUES (not change)
 
 default_laravel_port_local=8000
 default_laravel_port_docker=8000
@@ -22,8 +23,8 @@ default_laravel_volume=".my-project"
 
 #CUSTOM BITNAMI MARIADB VALUES
 
-custom_mariadb_user="bn_myapp_custom"
-custom_mariadb_database="bitnami_myapp_custom"
+custom_mariadb_user="jose"
+custom_mariadb_database="laravel_db"
 
 #CUSTOM BITNAMI LARAVEL VALUES 
 
@@ -41,11 +42,9 @@ sed -e "s/$default_mariadb_user/$custom_mariadb_user/" \
  	-e "s/\.$default_laravel_volume/$custom_laravel_volume/"  \
  	-e "s/$default_laravel_port_local:/$custom_laravel_port_local:/"  \
  	"$path_docker_compose_yml/docker-compose.yml" \
- 	> /dev/null
+ 	> $output_path
 
 echo "Docker compose file has been update in: '$output_path'"
-
-
 
 
 
